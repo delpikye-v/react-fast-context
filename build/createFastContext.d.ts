1aef758c8624c9efbc2bc3a38cedc4eeb0aeedfb
Delpi Kye (Tung Vu) <tungvuit@gmail.com>
2026-01-28 23:44:17 +0700

    chore: add merge modes and utility helpers

--- build/createFastContext.d.ts
+++ build/createFastContext.d.ts
@@ -1,12 +1,13 @@
 import type { Store } from "./core/store";
-import type { StateActionMap, FastContextProps } from "./core/types";
+import type { StateActionMap, FastContextProps, MergeMode } from "./core/types";
 type BoundActions<S, A extends StateActionMap<S>> = {
     [K in keyof A]: (...args: Parameters<A[K]> extends [any, ...infer P] ? P : never) => void;
 };
 type ResolveActions<S, A> = A extends StateActionMap<S> ? BoundActions<S, A> : undefined;
-type ComputedApi<S> = <R>(selector: (s: S) => R, equality?: (a: R, b: R) => boolean) => {
+type DerivedSelector<S> = <R>(selector: (s: S) => R, equality?: (a: R, b: R) => boolean) => {
     use(): R;
     value(): R;
+    subscribe(callback: (value: R) => void): () => void;
 };
 type FastContextReturn<S, A> = {
     state(): S;
@@ -15,12 +16,16 @@ type FastContextReturn<S, A> = {
     batch: Store<S>["batch"];
     transaction: Store<S>["transaction"];
     subscribe: Store<S>["subscribe"];
-    devtools: (cb: (info: {
+    devtools: (callback: (info: {
         state: S;
     }) => void) => () => void;
     use: <T = S>(selector?: (state: S) => T) => T;
-    computed: ComputedApi<S>;
-    scope(state: S): FastContextReturn<S, A>;
+    computed: DerivedSelector<S>;
+    select: DerivedSelector<S>;
+    when: <R>(predicate: (state: S) => R, effect: (value: R, state: S) => void) => () => void;
+    scope(state: S, options?: {
+        merge?: MergeMode;
+    }): FastContextReturn<S, A>;
     actions: ResolveActions<S, A>;
 };
 export declare function createFastContext<S extends object, A extends StateActionMap<S>>(props: FastContextProps<S, A>): FastContextReturn<S, A>;
--- build/index.d.ts
+++ build/index.d.ts
@@ -1,7 +1,7 @@
 export { createFastContext } from "./createFastContext";
 export { createStore } from "./core/store";
-export type { Store } from "./core/store";
 export { bindActions } from "./core/actions";
+export type { Store } from "./core/store";
 export * from "./core/types";
 export { createFastSelector } from "./react/fastSelector";
 export { bindFastContext } from "./react/bindFastContext";
